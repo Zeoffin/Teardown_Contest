326168a18a70abeee12384f0032b4c6da10aaa53
Zeo <markuss.skrastins@gmail.com>
2023-01-07 13:08:18 +0000

    Added working scanner

--- scripts/decompression.lua
+++ scripts/decompression.lua
@@ -1,20 +1,22 @@
 --- File that deals with the airScanners to check for holes in the spaceship and apply the effect of decompression
 --- Thanks to the guy who wrote Boeing 737 mod for the idea
 
+--- TODO: Query everything with tag rayCanPass to set as rejectBody
+
 function init()
-	DebugPrint("Decompressing stuff...")
-	airScanner = FindLocation("airScanner")	            --- Locations of the scanners
-	DebugPrint(airScanner)
-	pos = GetLocationTransform(airScanner).pos           --- Positional vector
-	DebugPrint(string.format("Scanner position: %d %d %d", pos[1], pos[2], pos[3]))
-	resolution = 24     							--- Still no clue wtf this is
-	pressure = 100                                  --- Pressure
+	airScanner = FindLocation("airScanner", true)	            	--- Locations of the scanners
+	scannerPosition = GetLocationTransform(airScanner).pos          --- Positional vector
+	scannerRotation = GetLocationTransform(airScanner).rot          --- Positional vector rotation
+	resolution = 12     											--- Number of star directions
+	pressure = 100                                 					--- Pressure
 	activePosList = {}                              --- so that each hole can be saved and constantly checked
 	activeDirList = {}                              --- List of all direction scanning
 	activeTimeList = {}                             --- smt ?
 	activated = false --changes when everything activates
 	--losePressure = not GetBool("savegame.mod.cabin.depressurize_inf",false)
 	timer = 0
+	rotationVal = 0
+
 end
 
 
@@ -23,29 +25,44 @@ function tick()
     --- TODO: Method of scanning the room. Requires a lot of scanners? Or not? Performance effects?
 	--- TODO: airScanner position 0,0,0 wtf ?
 
-	--DebugPrint(string.format("%d %d %d", pos[1], pos[2], pos[3]))
+	if rotationVal ~=360 then
+		rotationVal =  rotationVal + 5
+	else
+		rotationVal = 0
+	end
 
 	if pressure > 0 then
 		i = 0 --raycast counter
 
 		--- copy the pos of probe
-		local checkpos = VecCopy(pos)
+		local rayPosition = VecCopy(scannerPosition)
+		--local rayRotation = VecCopy(scannerRotation)
+
 
 		--- resolution ... ?
 		for r=0,resolution do
-			local quat = QuatEuler(0,0,r*360/resolution)	--- quaternion - basically kaut kas rotē wtf nezinu ?
-			local dir = TransformToParentPoint(Transform(Vec(0,0,0),quat),Vec(1,0,0))	--- iet kopa vnk ar lidmasinu ?
+			local quat = QuatEuler(0,0,r*360/resolution)
+			local dir = TransformToParentPoint(Transform(Vec(0,0,0),quat),Vec(1,0,0))
 
-			if activePosList[i] ~= checkpos and activeDirList[i] ~= dir then
-				--DebugLine(checkpos,VecAdd(checkpos,dir),0,0,1) --debug the current checked position
+			local rotationQuat = QuatEuler(rotationVal, rotationVal, rotationVal)
+			local rotatedVector = QuatRotateVec(rotationQuat, dir)
+
+
+			if activePosList[i] ~= rayPosition and activeDirList[i] ~= rotatedVector then
+
+				--DebugLine(rayPosition,VecAdd(rayPosition,dir),0,0,1) --debug the current checked position
+
 				i = i + 1
-				hit, dist = QueryRaycast(checkpos,dir,10)
-				endpoint = VecAdd(checkpos, VecScale(dir, dist))
-				DrawLine(checkpos, endpoint, 1, 0, 0)
-				--if not hit then
-				--	activePosList[i] = checkpos
-				--	activeDirList[i] = dir
-				--	activeTimeList[i] = 0
+				hit, dist = QueryRaycast(rayPosition,rotatedVector,10)
+				endpoint = VecAdd(rayPosition, VecScale(rotatedVector, dist))
+				DrawLine(rayPosition, endpoint, 1, 0, 0)
+				if not hit then
+					DebugPrint("FOUND HOLE")
+					activePosList[i] = rayPosition
+					activeDirList[i] = rotatedVector
+					activeTimeList[i] = 0
+
+				end
 			end
 		end
 	end
